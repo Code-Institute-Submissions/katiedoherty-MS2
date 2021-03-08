930d339316773779a2a19ad43afe3ff596ccfd46
katiedoherty <katie.doherty@mycit.ie>
2021-03-08 22:04:59 +0000

    got information returned from Google Places, need to figure out position property to get markers to work.

--- javascript/geolocation.js
+++ javascript/geolocation.js
@@ -2,37 +2,43 @@
  
  let marker;
   let map;
-  let Ireland;
   let service;
   let pos;
+
+
+ 
   
  function initMap(){
+     
+
+    var latmap = new google.maps.LatLng(53.41291,-8.24389);
+
       
             map = new google.maps.Map(document.getElementById("map-canvas"),{
-                zoom:15,
-                center:{
-                    lat:53.41291,
-                    lng:-8.24389
-                }
+                zoom:13,
+                center:latmap
             });
 
            
              if (navigator.geolocation){
 
             navigator.geolocation.getCurrentPosition(function(p){
+
+                pos={
+            lat: p.coords.latitude,
+            lng: p.coords.longitude,
+          }
                 
            marker = new google.maps.Marker({
                 map,
                 draggable: false,
                 animation: google.maps.Animation.DROP,
                 
-                position: {
-            lat: p.coords.latitude,
-            lng: p.coords.longitude,
-          },});
+                position: pos});
+         
           
           marker.setAnimation(google.maps.Animation.BOUNCE);
-          
+          getRestaurants(pos);
           
 
             const contentString= `<p>You Are Here</p>`
@@ -45,34 +51,48 @@
     infowindow.open(map, marker)
   );
 
-  getRestaurants(location);
+  
+  
   
             });
+            
         
         }
 
         
              }
 
-             function getRestaurants(position){
-                 var pyrmont= new google.maps.LatLng(location.lat, location.long);
+             function getRestaurants(pos){
+                 var pyrmont= new google.maps.LatLng(pos.lat, pos.lng);
                  var request={
                      location: pyrmont,
-                     radius:1500,
+                     radius:500,
                      type:["restaurant"]
                  };
 
                  service = new google.maps.places.PlacesService(map);
-                 service.nearbySearch(request, callback);
+                service.nearbySearch(request, callback);
+             }
+
+              function createMarker(){
+                 
+                new google.maps.Marker({
+                map,
+                draggable: false,
+                animation: google.maps.Animation.DROP,
+                
+                position: pos.request.callback});
              }
 
              function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+    for (var i = 0; i < results.length; i++) 
+    {
+     createMarker(results[i]);
     }
-   
+  
   }
+
 }
 
 
